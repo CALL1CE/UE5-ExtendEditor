ce5798a2d45301c2361f0c322de7ce9bcb61e542
CALL1CE <563638083@qq.com>
2024-11-12 21:14:26 +0800

    002 Editor Utility Widget & 003 Build An Editor Utility Widget

---
+++ ExtendEditor/Plugins/SuperManager/Source/SuperManager/Private/AssetActions/QuickMaterialCreationWidget.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AssetActions/QuickMaterialCreationWidget.h"
+#include "DebugHeader.h"
+
+#pragma region QuickMaterialCreationCore
+
+void UQuickMaterialCreationWidget::CreateMaterialFromSelectedTextures()
+{
+	DebugHeader::Print(TEXT("working..."), FColor::Cyan);
+}
+#pragma endregion QuickMaterialCreationCore
